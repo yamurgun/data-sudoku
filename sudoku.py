54cf6c2123fe1d4a2fc2f6e6551da0de2ecf8999
yamurgun <guneryagmurr@gmail.com>
2026-05-05 23:56:43 +0300

    Implement sudoku validator

--- sudoku.py
+++ sudoku.py
@@ -2,4 +2,29 @@
 
 
 def sudoku_validator(grid):
-    pass  # YOUR CODE HERE
+    expected = set(range(1,10))
+
+    for row in grid:
+        if set(row) != expected:
+            return False
+
+    for col_index in range(9):
+        column = []
+        for row_index in range(9):
+            column.append(grid[row_index][col_index])
+
+        if set(column) != expected:
+            return False
+
+    for box_row in range(0, 9, 3):
+        for box_col in range(0, 9, 3):
+            box = []
+
+            for row_index in range(box_row, box_row + 3):
+                for col_index in range(box_col, box_col +3):
+                    box.append(grid[row_index][col_index])
+
+            if set(box) != expected:
+                return False
+
+    return True
